c49b29c5abdd70b6ca3f76d8657159b15d79cadb
Alp Enes Şeker <0alp.enes.seker@gmail.com>
2025-05-05 21:08:46 +0300

    biter

--- Assets/Scripts/FinishTrigger.cs
+++ Assets/Scripts/FinishTrigger.cs
@@ -56,6 +56,7 @@ public class FinishTrigger : MonoBehaviour
                     if (gameControl != null)
                     {
                         gameControl.enabled = true;
+                        movement.enabled = true;
                     }
                 }
             }
